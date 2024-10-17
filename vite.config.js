c63cf2e47bcc38415aec391501392d16fbd57e3b
surya12876 <104568391+surya12876@users.noreply.github.com>
2024-10-17 18:46:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '/src/main.jsx' // Add the module that should not be bundled
+      ]
+    }
+  }
 })
